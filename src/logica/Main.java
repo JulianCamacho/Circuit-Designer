56482b93aecb41950a718e71f1b4872c972ae865
Julián Camacho <jjulian.341@gmail.com>
2019-09-04 01:09:31 -0600

    Métodos de inicio

---
+++ src/logica/Main.java
@@ -0,0 +1,18 @@
+package logica;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ * Metodo principal de la aplicacion
+ * @author Jose Julian Camacho
+ * @date 4.9.19
+ * */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+}
